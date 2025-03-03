43900c17b405f8a83b3721822ae436f3e15c6824
AbiNugro <abinugroho2901@gmail.com>
2025-03-03 23:15:18 +0700

    Add files via upload

---
+++ Pewarisan/src/pewarisan/A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pewarisan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class A {
+    int x;
+    int y;
+    
+    void TampilkanNilaiXY() {
+        System.out.println("Nilai x : " + x +", y : " + y);
+    }
+}
